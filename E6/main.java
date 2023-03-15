5930f0aa7e61e5b4ee48425acc01d8aa9c7c932f
martincorona007 <martincorona007@hotmail.com>
2023-03-14 21:22:19 -0600

    not much

--- E6/main.java
+++ E6/main.java
@@ -1,7 +1,7 @@
 public class main{
     public static void main(String[] args){
         int a = 0;
-        int b = 0;
+        int b = 0; 
         
         System.out.println("[Pre-increment a ]");
         ++a;
