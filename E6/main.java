021e70619362e167a44f8b8fb40af9542a13a869
martincorona007 <martincorona007@hotmail.com>
2023-03-15 17:39:19 -0600

    3/15/2023

--- E6/main.java
+++ E6/main.java
@@ -1,5 +1,13 @@
 public class main{
     public static void main(String[] args){
+        int dec = 152;
+        int bin = 0b10011000;
+        int oct = 0230;
+        int hex = 0x98;
+        System.out.println(dec);
+        System.out.println(bin);
+        System.out.println(oct);
+        System.out.println(hex);
         int a = 0;
         int b = 0; 
         
