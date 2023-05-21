6bb2c0a0303ea7815717cdf4dd77be6d4a007442
martincorona007 <martincorona007@hotmail.com>
2023-05-21 16:56:05 -0600

    adding project intellij and eclipse

---
+++ EM/eclipse/testing-code/src/codewars/SumArray.java
@@ -0,0 +1,12 @@
+package codewars;
+
+public class SumArray {
+
+	  public static double sum(double[] numbers) {
+	   double totalSum = 0;
+	    for(int i = 0; i < numbers.length; i++){
+	      totalSum +=numbers[i]; 
+	    }
+	   return (totalSum == 0)? 0 : totalSum;
+	  }
+}
