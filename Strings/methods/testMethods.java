f5fbd92c9de2845c4c0f1a920bb772fbf32ba1c3
martincorona007 <martincorona007@hotmail.com>
2025-04-08 17:32:54 -0600

    04082025

---
+++ Strings/methods/testMethods.java
@@ -0,0 +1,10 @@
+public class testMethods{
+  public static void main(String []args){
+    String testIdea1 = "I am going to the park ";
+    System.out.println(""+testIdea1.toUpperCase());
+    System.out.println(""+testIdea1.trim());
+    System.out.println(""+testIdea1.toLowerCase());
+    System.out.println(""+testIdea1.contains(""));
+
+  }
+}
